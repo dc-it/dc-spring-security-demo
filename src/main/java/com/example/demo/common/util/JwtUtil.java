02445429ea67acbbe45406c5882555ea14926ddd
duchaoyun <1255604509@qq.com>
2020-10-29 18:16:19 +0800

    无状态

---
+++ src/main/java/com/example/demo/common/util/JwtUtil.java
@@ -0,0 +1,17 @@
+package com.example.demo.common.util;
+
+import com.nimbusds.jwt.JWT;
+import lombok.experimental.UtilityClass;
+
+/**
+ * jwt工具类
+ *
+ * @author duchao
+ */
+@UtilityClass
+public class JwtUtil {
+
+    void test(){
+        //JWT
+    }
+}
